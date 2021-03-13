439d9446662118fafc17c7efecc759908b077a5f
shimin <shishishishimin@gmail.com>
2021-03-14 03:37:34 +0800

    initialize login page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="bg" :style="{ backgroundImage: 'url(' + image + ')' }">
+      <div class="welcomebox">
+        <p>Welcome!</p>
+        <textarea v-model="email" placeholder="email"></textarea> <br>
+        <textarea v-model="password" placeholder="password"></textarea> <br>
+        <button> Login </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      image:
+        "https://www.kidsofbath.com/wp-content/uploads/2019/02/rainbow-woods-kidsofbath.jpg",
+    };
+  },
+};
 </script>
 
 <style>
-#app {
+.app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.bg {
+  height:100vh;
+  background-size: cover;
+  overflow: hidden;
+  opacity: 85%;
+}
+
+.welcomebox {
+  margin: 20%;
+  background-color: rgba(0, 0, 0, 0.82);
+  color: azure;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
