a0fee40f1f8f2df77200f02cefa9335473144fca
junwei <e0420344@u.nus.edu>
2021-03-19 03:03:13 +0800

    Updated BasePage, HomePage

--- src/components/BasePage.vue
+++ src/components/BasePage.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="bg">
-    <div class="navbar">
-      <nav>
-        <button id="btn"><router-link to="/home"><img src="../assets/homelogo.png"></router-link></button>
-        <button id="btn"><router-link to="/map"><img src="../assets/maplogo.png"></router-link></button>
-        <button id="btn"><router-link to="/news"><img src="../assets/newslogo.png"></router-link></button>
-        <button id="btn"><router-link to="/quiz"><img src="../assets/quizlogo.png"></router-link></button>
-      </nav>
+  <div>
+    <div class="bg">
+      <span>
+        <img id="logoIcon" src="../assets/logo.png">
+        <a id="ggName">GrowGreen</a>
+      </span>
+      <ul class="navbar">
+        <li><router-link to="/home">HOME</router-link></li>
+        <li><router-link to="/map">MAP</router-link></li>
+        <li><router-link to="/news">NEWS</router-link></li>
+        <li><router-link to="/quiz">QUIZ</router-link></li>
+        <li><router-link to="/dashboard">OVERVIEW</router-link></li>
+        <!-- <input type="submit" id="profileIcon" src="../assets/profileIcon.png"/> -->
+        <img id="profileIcon" src="../assets/profileIcon.png" @mouseover="mouseOver">
+        <div v-show="hover">Profile<br> Log Out</div>
+      </ul>
     </div>
   </div>
 </template>
@@ -17,26 +25,58 @@ export default {
   name: "App",
   data: function () {
     return {
+      hover: false
     };
   },
+  methods: {
+    mouseOver() {
+      this.hover = !this.hover;
+    }
+  }
 };
 </script>
 
 <style scoped>
 .bg {
-  height:100vh;
-  background-size: cover;
-  overflow: hidden;
-  background-color: antiquewhite;
+  background-color: ivory;
+  background-size: over;
+  background-repeat: no-repeat;
+}
+#ggName {
+  position: fixed;
+  top: 45px;
+  font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  font-size: 30px;
+  font-weight: 600;
+  color: #873600;
+}
+#logoIcon {
+  height: 70px;
+  padding: 20px;
+  float: left;
 }
 .navbar {
-  background-color: black;
-  opacity: 82%;
-  text-align: center;
+  display: flex;
+  float: right;
+  padding-right: 35px;;
+  flex-wrap: wrap;
+  list-style-type: none;
 }
-
-#btn {
-  background-color: black;
-  border:0px;
+.navbar > li {
+  padding: 10px;
+  padding-top: 20px;
+  margin: 10px;
+}
+.navbar > li > a {
+  text-decoration: none;
+  color: black;
+}
+.navbar > li > a:hover {
+  color: grey;
+}
+#profileIcon {
+  height: 60px;
+  padding-left: 20px;
+  padding-top: 10px;
 }
 </style>
--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -64,7 +64,7 @@ export default {
 p {
   font-family: Playfair;
   font-weight: bold;
-  font-size: 20px;;
+  font-size: 20px;
 }
 .textinput{
   background: transparent;
